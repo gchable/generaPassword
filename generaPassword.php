5a260b1607c11422d4597e51d2df975365e4b3aa
Gonzalo <gonzalo_conta@hotmail.com>
2020-03-04 13:03:23 -0500

    pasar html al final,  se pasa como argumento a la funcion, el  debe ser el largo de  para volver a iniciar

--- generaPassword.php
+++ generaPassword.php
@@ -10,13 +10,13 @@ $caracteres = [
 if (isset($_POST['submit'])) {
     $largo = $_POST['largo'];
 
-    function genera($largo)
+    function genera($largo, $caracteres)
     {
         $contador = 0;
         $pass = "";
 
         for ($i = 1; $i <= $largo; $i++) {
-            if ($contador == 4) {
+            if ($contador == count($caracteres)) {
                 $contador = 0;
             }
 
@@ -26,35 +26,26 @@ if (isset($_POST['submit'])) {
         }
         return str_shuffle($pass);
     }
-
 }
-?>
-<?php
+
 $largoSolicitado = "";
-if (isset($largo)) {$largoSolicitado = "(" . $largo . ")";}
-;
+if (isset($largo)) {$largoSolicitado = "(" . $largo . ")";};
 echo "<h1>Generar contraseña" . $largoSolicitado . "</h1>";
+
+echo "<p>Patrón:<br>
+            Mayúsculas: " . $caracteres[0] . "<br>
+            Minúsculas: " . $caracteres[1] . "<br>
+            Números: " . $caracteres[2] . "<br>
+            Símbolos: " . $caracteres[3] . "<br><br>
+    ";
+
+if (isset($largo)) {
+    echo "<h2>" . genera($largo, $caracteres) . "</h2>";
+}
+
 ?>
+
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
    <input type="number" name="largo" min="5" max="20" style="width: 180;" required placeholder="Largo de la contraseña">
    <input type="submit" name="submit" value="Genera">
 </form>
-
-<?php
-echo "<p>Patrón:<br>
-            Mayúsculas: ".$caracteres[0]."<br>
-            Minúsculas: ".$caracteres[1]."<br>
-            Números: ".$caracteres[2]."<br>
-            Símbolos: ".$caracteres[3]."<br><br>
-    ";
-?>
-
-<?php
-if (isset($largo)) {
-    echo "<h2>" . genera($largo) . "</h2>";
-}
-
-?>
-
-
-
