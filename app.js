b595bc67b53be78c06c29086ba7a0993df3deb4d
Gonzalo Chable <gonzalo_conta@hotmail.com>
2023-08-28 09:32:24 -0500

    varios varios cambios

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-var formulario = document.querySelector('#formulario');
-var respuesta = document.querySelector('#resultado');
-
+const formulario = document.getElementById('formulario');
+const respuesta = document.getElementById('resultado');
 formulario.addEventListener("submit", function (e) {
     e.preventDefault();
     var datos = new FormData(formulario);
@@ -11,14 +10,18 @@ formulario.addEventListener("submit", function (e) {
     })
         .then(res => res.json())
         .then(data => {
-            if (data === "error") {
-                respuesta.innerHTML = `
-                    <div class="col s12 m6 section" id="valorResultado">
-                        Error...
-                    </div>
-                `;
-            } else {
-                respuesta.innerHTML = `${data}`;
-            }
+            respuesta.innerHTML = `<div class="col s10" id="copiar">${data.message}</div><a class="btn-floating pulse" onclick="myFunction()"><i class="material-icons">content_copy</i></a>`;
         })
 });
+
+function myFunction() {
+    str = document.getElementById('copiar').innerHTML;
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    // alert('Copied the text:' + el.value);
+    M.toast({html: 'Se ha copiado al portapapeles!'})
+}
--- generaPassword.php
+++ generaPassword.php
@@ -1,37 +1,64 @@
 <?php
 
+/**
+ * Se omiten ~ ´ ¨ [] {} | etc ya que son dificiles de escribir
+ * Se omite la letra o, ya que se confunde con 0 (cero)
+ * Se omite la letra i ya que se confude con 1 (uno)
+ * Se omite el número 0 ya que se confunde on o (ó)
+ * Se omite el número 1 ya que se confunde on l (ele)
+ */
 
 if (!empty($_POST['largo'])) {
 
-    $caracteres = [
-        "ABCDEFGHJKMNPQRSTUVWXYZ",
-        "abcdefghjkmnpqrstuvwxyz",
-        "23456456789",
-        "+-.*,/;=_",
-    ];
+   $largo = $_POST['largo'];
 
-    $largo = $_POST['largo'];
+   if ($largo < 4) {
 
-    function genera($largo, $caracteres)
-    {
-        $contador = 0;
-        $pass = "";
+      $response["status"] = "error";
+      $response["message"] = "El password debe ser mayor a 3";
+   } else {
 
-        for ($i = 1; $i <= $largo; $i++) {
-            if ($contador == count($caracteres)) {
-                $contador = 0;
+      $patrones = [
+         'mayusculas' => ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+         'minusculas' => ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+         'numeros' => [2, 3, 4, 5, 6, 4, 5, 6, 7, 8, 9],
+         'signos' => ['+', '-', '.', '*', ',', '/', ';', '=', '_', '(', ')']
+      ];
+
+      $totalCaracteres = 0;
+      foreach (array_keys($patrones) as $patron) {
+         $totalCaracteres += count($patrones[$patron]);
+      }
+
+      function generaPassword($largo, $patrones)
+      {
+         $password = array();
+         for ($i = 0; $i < $largo; $i += count($patrones)) {
+            foreach (array_keys($patrones) as $patron) {
+               shuffle($patrones[$patron]);
+               if ($patrones[$patron]) {
+                  $password[] = $patrones[$patron][0];
+                  unset($patrones[$patron][0]);
+               }
             }
-            $aleatorio = rand(0, strlen($caracteres[$contador]) - 1);
-            $pass .= $caracteres[$contador][$aleatorio];
-            $contador++;
-        }
-        return str_shuffle($pass);
-    }
-    
-    $pass = genera($largo, $caracteres);
-    echo json_encode("
-        <div class='col s12 m6 section'>
-            {$pass}
-        </div>
-    ");
+         }
+
+         shuffle($password);
+         return implode($password);
+      }
+
+      $password = generaPassword($largo, $patrones);
+
+      do {
+         $password = str_pad($password, $largo, $password);
+         $password = substr($password, 0, $largo);
+         $password = str_shuffle($password);
+         $totalCaracteres++;
+      } while ($totalCaracteres < $largo);
+
+      $response["status"] = "success";
+      $response["message"] = $password;
+   }
+
+   echo json_encode($response);
 }
